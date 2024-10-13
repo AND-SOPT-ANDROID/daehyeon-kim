37a693b7dd2725724253333a728b274435d82b0b
wjdrjs00 <kimjk3409@gmail.com>
2024-10-14 01:33:37 +0900

    fix: wrapContentHeight()활용해서 높이 고정 수정

--- app/src/main/java/org/sopt/and/ui/signin/SignInScreen.kt
+++ app/src/main/java/org/sopt/and/ui/signin/SignInScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -152,7 +153,7 @@ fun SignInContent(
             colors = ButtonDefaults.buttonColors(Color.Blue),
             modifier = Modifier
                 .fillMaxWidth()
-                .height(50.dp)
+                .wrapContentHeight()
         ) {
             Text(
                 text = "로그인",
