61dc82ccfc559e84452de9e43aa5c01fb5d16d7c
wjdrjs00 <kimjk3409@gmail.com>
2024-10-19 16:18:19 +0900

    #3 feat: 키보드 등장시 화면 올라가도록 구현
    - manifest -> android:windowSoftInputMode="adjustResize" 추가
    - modifier -> imePadding() 추가

--- app/src/main/java/org/sopt/and/ui/signup/SignUpScreen.kt
+++ app/src/main/java/org/sopt/and/ui/signup/SignUpScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
@@ -59,6 +60,7 @@ fun SignUpScreen(
     }
 
     Scaffold(
+        modifier = Modifier.imePadding(),
         topBar = { SignUpTopBar() },
         bottomBar = {
             SignUpBottomBar(
