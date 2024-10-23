8e48c213566b1b421d0cd55fb077489fcdc95286
wjdrjs00 <kimjk3409@gmail.com>
2024-10-23 17:50:21 +0900

    feat(main): bottom navigator 컴포넌트 추가
    - Route 경로 추가
    - MainTab 클래스 추가
    - 아이콘 추가

--- app/src/main/java/org/sopt/and/core/navigation/Route.kt
+++ app/src/main/java/org/sopt/and/core/navigation/Route.kt
@@ -2,16 +2,24 @@ package org.sopt.and.core.navigation
 
 import kotlinx.serialization.Serializable
 
-@Serializable
-data class SignIn(
-    val email: String,
-    val password: String,
-)
+sealed interface Route {
+    @Serializable
+    data class SignIn(
+        val email: String = "",
+        val password: String = ""
+    ) : Route
 
-@Serializable
-data object SignUp
+    @Serializable
+    data object SignUp : Route
+}
 
-@Serializable
-data class My(
-    val email: String
-)
+sealed interface MainTabRoute : Route {
+    @Serializable
+    data object Home : MainTabRoute
+
+    @Serializable
+    data object Search : MainTabRoute
+
+    @Serializable
+    data object My : MainTabRoute
+}
