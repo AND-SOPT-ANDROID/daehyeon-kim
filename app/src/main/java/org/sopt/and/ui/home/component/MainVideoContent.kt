e9492d19afc4c71dbca3bb23ceecf54f63764de4
wjdrjs00 <kimjk3409@gmail.com>
2024-10-31 11:57:51 +0900

    feat(home): Pager 자동 스크롤 기능 추가
    - MainVideoContent 이미지 자동 스크롤을 위한 기능 추가

--- app/src/main/java/org/sopt/and/ui/home/component/MainVideoContent.kt
+++ app/src/main/java/org/sopt/and/ui/home/component/MainVideoContent.kt
@@ -8,12 +8,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,6 +25,7 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 import org.sopt.and.R
 import org.sopt.and.model.Video
 
@@ -34,6 +35,16 @@ fun MainVideoContent(
     modifier: Modifier = Modifier
 ) {
     val pagerState = rememberPagerState(pageCount = { mainVideoList.size })
+
+    LaunchedEffect(true) {
+        while (true) {
+            delay(4000L)
+            if (!pagerState.isScrollInProgress) {
+                val nextPage = (pagerState.currentPage + 1) % mainVideoList.size
+                pagerState.animateScrollToPage(nextPage)
+            }
+        }
+    }
 
     Column(
         modifier = modifier
