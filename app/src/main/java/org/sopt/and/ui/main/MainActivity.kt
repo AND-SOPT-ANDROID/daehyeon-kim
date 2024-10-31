6d38f7791fc0b5a7d4598acceaf236f7fcdd01c2
wjdrjs00 <kimjk3409@gmail.com>
2024-10-31 15:10:28 +0900

    feat(main): 상태 표시줄 색상 변경

--- app/src/main/java/org/sopt/and/ui/main/MainActivity.kt
+++ app/src/main/java/org/sopt/and/ui/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import org.sopt.and.core.designsystem.theme.ANDANDROIDTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,6 +12,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            WindowCompat.getInsetsController(window, window.decorView).apply {
+                isAppearanceLightStatusBars = false
+            }
+
             ANDANDROIDTheme {
                 MainScreen()
             }
