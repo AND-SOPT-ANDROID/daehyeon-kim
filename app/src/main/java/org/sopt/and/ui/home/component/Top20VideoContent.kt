2262656756550cb16feedc70ed812fd8f6497356
wjdrjs00 <kimjk3409@gmail.com>
2024-10-31 17:02:20 +0900

    refactor(home): TopRankVideo 이미지 컴포넌트화

--- app/src/main/java/org/sopt/and/ui/home/component/Top20VideoContent.kt
+++ app/src/main/java/org/sopt/and/ui/home/component/Top20VideoContent.kt
@@ -1,28 +1,18 @@
 package org.sopt.and.ui.home.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,34 +51,7 @@ fun Top20VideoContent(
                 items = top20Videos,
                 key = { item: Top20Video -> item.videoId }
             ) { top20Videos ->
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.BottomStart
-                ) {
-                    Column {
-                        Image(
-                            painter = painterResource(top20Videos.image),
-                            contentDescription = "",
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 5.dp)
-                                .clip(
-                                    RoundedCornerShape(10.dp)
-                                )
-                            ,
-                            contentScale = ContentScale.Crop
-                        )
-                        Spacer(modifier = Modifier.weight(0.15f))
-                    }
-                    Text(
-                        text = "${top20Videos.rank}",
-                        modifier = Modifier.padding(start = 5.dp),
-                        fontSize = 60.sp,
-                        fontStyle = FontStyle.Italic,
-                        fontWeight = FontWeight.ExtraBold,
-                        color = Color.White
-                    )
-                }
+                TopRankVideoImage(top20Videos)
             }
         }
     }
